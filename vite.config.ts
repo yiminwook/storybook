b7b2ae3c3a4e2e110e2fa742bd9f6854a8363f1e
yiminwook <grs0416@gmail.com>
2024-08-04 23:28:26 +0900

    #DEV#NEW - gh pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import svgr from "vite-plugin-svgr";
@@ -6,8 +6,7 @@ import path from "path";
 import dts from "vite-plugin-dts";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const defaultConfig: UserConfig = {
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
@@ -19,33 +18,49 @@ export default defineConfig({
       identifiers: ({ hash }) => `css_${hash}`,
     }),
     svgr(),
-    dts({
-      tsconfigPath: "./tsconfig.build.json",
-    }),
-    viteStaticCopy({
-      targets: [{ src: "src/index.css", dest: "" }],
-    }),
   ],
   build: {
-    lib: {
-      name: "@twosday/ui",
-      entry: path.resolve(__dirname, "src/index.tsx"),
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-        // assetFileNames: (assetInfo) => {
-        //   if (assetInfo.name === "style.css") return "custom.css";
-        //   return assetInfo.name as string;
-        // },
-      },
-    },
-    sourcemap: true,
-    emptyOutDir: true,
+    outDir: "build",
+    emptyOutDir: true, // 빌드시 기존 파일 삭제
   },
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+  if (process.env.DEPLOY_TARGET === "npm") {
+    defaultConfig.plugins?.push(
+      dts({
+        tsconfigPath: "./tsconfig.build.json",
+      }),
+    );
+    defaultConfig.plugins?.push(
+      viteStaticCopy({
+        targets: [{ src: "src/index.css", dest: "" }],
+      }),
+    );
+    defaultConfig.build = {
+      lib: {
+        name: "@twosday/ui",
+        entry: path.resolve(__dirname, "src/index.tsx"),
+        fileName: (format) => `index.${format}.js`,
+      },
+      rollupOptions: {
+        external: ["react", "react-dom"],
+        output: {
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
+          // assetFileNames: (assetInfo) => {
+          //   if (assetInfo.name === "style.css") return "custom.css";
+          //   return assetInfo.name as string;
+          // },
+        },
+      },
+      sourcemap: true,
+      emptyOutDir: true,
+    } satisfies UserConfig["build"];
+  }
+
+  return defaultConfig;
 });
