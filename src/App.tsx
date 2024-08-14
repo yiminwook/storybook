6a1a767b7dd1dc0cae81fc19d38679e171fdec63
yiminwook <grs0416@gmail.com>
2024-08-14 21:50:00 +0900

    #TST#NEW - vanilla js

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Gnb from "./component/Gnb/Gnb";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import GnbItem from "./component/GnbItem";
 
 const Home = React.lazy(() => import("./page/home"));
 const Preview = React.lazy(() => import("./page/preview"));
@@ -10,23 +10,24 @@ const Vanilla = React.lazy(() => import("./page/vanilla"));
 export default function App() {
   return (
     <BrowserRouter>
-      <Gnb />
+      <aside>
+        <h1>
+          <Link to="/">
+            UI <sub>design</sub>
+          </Link>
+        </h1>
+        <ul className="mainRoutes">
+          <GnbItem name="test1" href="/test1" />
+          <GnbItem name="vanilla" href="/vanilla" />
+        </ul>
+      </aside>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<Preview />} />
           <Route path="/test1" element={<Test1 />} />
           <Route path="/vanilla" element={<Vanilla />} />
-          <Route
-            path="*"
-            element={
-              <div>
-                <div>
-                  <a href="/">홈으로 돌아가기</a>
-                </div>
-              </div>
-            }
-          />
+          <Route path="*" element={<div>404</div>} />
         </Routes>
       </Suspense>
     </BrowserRouter>
--- src/page/vanilla.tsx
+++ src/page/vanilla.tsx
@@ -1,7 +1,24 @@
 import VanillaWrapper from "@/component/VanillaWrapper";
 
-const initiator = ($div: HTMLDivElement) => {
-  $div.textContent = "hello world!";
+const initiator = ($wrapper: HTMLDivElement) => {
+  let state = false;
+
+  const $p = document.createElement("p");
+  $p.textContent = "꺼짐";
+
+  const $button = document.createElement("button");
+  $button.textContent = "토글";
+  $button.addEventListener("click", () => {
+    state = !state;
+    $p.textContent = state ? "켜짐" : "꺼짐";
+  });
+
+  const $box = document.createElement("div");
+  $box.textContent = "테스트2 바닐라";
+  $box.append($p);
+  $box.append($button);
+
+  $wrapper.insertAdjacentElement("beforeend", $box);
 };
 
 export default function Vanilla() {
